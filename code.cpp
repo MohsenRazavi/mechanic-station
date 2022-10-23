1898308ee37c0ebdb20815b5b90b9de67a48e736
mohsen razavi <mohsenrazavi.self@gmail.com>
2022-10-23 21:31:39 +0330

    add some features

--- code.cpp
+++ code.cpp
@@ -6,33 +6,44 @@ using namespace std;
 
 class Worker{
     public:
-    string name;
-    string job;
-    double peyment;
+        string name;
+        string job;
+        double peyment;
 
-    Worker(string name, string job, double payment):name(name),job(job),peyment(peyment){};
+        explicit
+            Worker(string name, string job, double payment):name(name),job(job),peyment(peyment){};
 
-    void work(){
-        cout<<"I'm workiiingggg !!!";
-    }
+        void work(){
+            cout<<"I'm workiiingggg !!!";
+        }
 };
+
 class Customer{
     public:
-    string name;
-    string car_name;
-    string probliem_detail;
-    double prices[MAXCAP];
-    string tools[MAXCAP];
+        string name;
+        string car_name;
+        string problem_detail;
+        double prices[MAXCAP];
+        string tools[MAXCAP];
+        
+        explicit
+            Customer(string name, string car_name, string problem_detail):name(name), car_name(car_name), problem_detail(problem_detail){};
 
-    void add_price(double new_price){
-        prices[sizeof(prices)+1] = new_price;
-    }
+        void add_price(double new_price){
+            prices[sizeof(prices)+1] = new_price;
+        }
 
-    void add_tool(string new_tool_name){
-        tools[sizeof(tools)+1] = new_tool_name;
-    }
+        void add_tool(string new_tool_name){
+            tools[sizeof(tools)+1] = new_tool_name;
+        }
+
 };
-class MechanicStation{};
+
+class MechanicStation{
+    string station_name;
+    string station_owner;
+    
+};
 
 
 int main(){
